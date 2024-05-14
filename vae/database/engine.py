a6433853d86d1bbf798abccff19c3edf10fef798
angelcruzlasso <angelcruzlasso@gmail.com>
2024-05-14 10:01:28 -0500

    prueba consultando a la base de datos

--- vae/database/engine.py
+++ vae/database/engine.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
-from database.dbconfig import url_object
+from dbconfig import url_object
 
 engine = create_engine(url_object)
--- vae/database/models.py
+++ vae/database/models.py
@@ -1,16 +1,13 @@
-from sqlalchemy import Session, Column, String, Float, BigInteger, Integer
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Float, BigInteger, Integer
 from sqlalchemy.orm import sessionmaker
-from database.engine import engine
+import engine
 
 # Definir la sesión y la base
 Session = sessionmaker(bind=engine) # Crear la sesión  # noqa: F811
 session = Session() # Crear la sesión
-Base = declarative_base() # Crear la base
-
 
 # Modelo de la tabla journal
-class Journal(Base):
+class Journal:
     __tablename__ = 'journal' # Nombre de la tabla
 
     journal_id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False) # Columna journal_id
@@ -21,3 +18,20 @@ class Journal(Base):
 
     def __repr__(self):
         return f"<journal_id=(id={self.journal_id}, path='{self.path}', seq='{self.seq}',primary_locale='{self.primary_locale}',enabled='{self.enabled}'))>" # Representación de la tabla
+ 
+def consultar_tabla ():
+        path = str
+        with Session() as session:
+            try:
+                registros = session.query(Journal).filter(Journal.path == path).all()
+                for registro in registros:
+                    print(registro)
+                return None
+            except Exception as e:
+                print(f"ocurrio un error inesperado: {e}")
+                return None
+            finally:
+                session.close()
+consultar_tabla()
+            
+
