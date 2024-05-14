f8914dd2d0af836228da562133eb11fa2ce5072d
raul serrano <raul.serrano01@up.ac.pa>
2024-05-14 10:41:02 -0500

    se planteo del modelo de Journals

--- vae/database/engine.py
+++ vae/database/engine.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
 from dbconfig import url_object
 
-engine = create_engine(url_object)
+engine = create_engine(url_object,echo=True)
+
--- vae/database/models.py
+++ vae/database/models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, String, Float, BigInteger, Integer
-from sqlalchemy.orm import sessionmaker
-import engine
+from sqlalchemy.orm import sessionmaker ,declarative_base
+from engine import engine
 
 # Definir la sesión y la base
 Session = sessionmaker(bind=engine) # Crear la sesión  # noqa: F811
 session = Session() # Crear la sesión
-
+Base    = declarative_base() # Crear la base
 # Modelo de la tabla journal
-class Journal:
-    __tablename__ = 'journal' # Nombre de la tabla
+class Journals(Base):
+    __tablename__ = 'journals' # Nombre de la tabla
 
     journal_id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False) # Columna journal_id
     path = Column(String(32), nullable=False) # Columna path
@@ -19,19 +19,17 @@ class Journal:
     def __repr__(self):
         return f"<journal_id=(id={self.journal_id}, path='{self.path}', seq='{self.seq}',primary_locale='{self.primary_locale}',enabled='{self.enabled}'))>" # Representación de la tabla
  
-def consultar_tabla ():
-        path = str
-        with Session() as session:
-            try:
-                registros = session.query(Journal).filter(Journal.path == path).all()
-                for registro in registros:
-                    print(registro)
-                return None
-            except Exception as e:
-                print(f"ocurrio un error inesperado: {e}")
-                return None
-            finally:
-                session.close()
+def consultar_tabla():
+    with Session() as session:
+        try:
+            registros = session.query(Journals).all()
+            for registro in registros:
+                print(registro.path)  # Imprime solo la columna path
+            return None
+        except Exception as e:
+            print(f"ocurrio un error inesperado: {e}")
+            return None
+
 consultar_tabla()
             
 
