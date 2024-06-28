010c0e2420bee38ffb3fbeba62abd2330db46e85
arcolinuxz <arcolinuxinfo@gmail.com>
2024-06-28 01:40:54 -0500

    se agrego un  engine que funciona por ssh , atraves de la vpn y simplemente en localhost cuando esta en local permientiendo utilizar el entorno real para trabajar desde cualquier lugar .

--- vae/database/dbconfig.py
+++ vae/database/dbconfig.py
@@ -1,11 +1,44 @@
+from sshtunnel import SSHTunnelForwarder
 from sqlalchemy.engine import URL
 
-# Configuración de la conexión MySQL
+# Configuración de la conexión SSH "eliminar esto en caso de ejecutar en local XD"
+SSH_HOST = '10.0.1.235'
+SSH_USER = 'edgar.perez'
+SSH_PASSWORD = 'eperez2022'
 
-url_object = URL.create(
-    "mysql+pymysql",
-    username="ojs",  
-    password="prueba123",
-    host="localhost",
-    database="ojs",
-)
+# Configuración de la conexión MySQL
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'ojs'
+MYSQL_PASSWORD = 'prueba123'
+MYSQL_DB = 'ojs'
+MYSQL_PORT = 3306
+
+# Configuración de la conexión MySQL
+try:
+    # Configura el túnel SSH
+    tunnel = SSHTunnelForwarder(
+        (SSH_HOST, 22),
+        ssh_username=SSH_USER,
+        ssh_password=SSH_PASSWORD,
+        remote_bind_address=(MYSQL_HOST, MYSQL_PORT)
+    )
+    tunnel.start()
+    
+    # Obtiene el puerto local del túnel
+    local_port = tunnel.local_bind_port
+
+    # Configura la URL de conexión a MySQL
+    url_object = URL.create(
+        "mysql+pymysql",
+        username=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        host="127.0.0.1",
+        port=local_port,
+        database=MYSQL_DB,
+    )
+
+except (Exception) as e:
+    print(f"Error al establecer la conexión: {e}")
+    if 'tunnel' in locals():
+        tunnel.stop()
+    raise e
--- vae/database/engine.py
+++ vae/database/engine.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from database.dbconfig import url_object
 
-engine = create_engine(url_object,)# echo=True se agrega para ver las consultas SQL que se ejecutan
+engine = create_engine(url_object)# echo=True se agrega para ver las consultas SQL que se ejecutan
 
