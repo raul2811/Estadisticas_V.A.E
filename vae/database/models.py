78e5b6e1ddd3b3102eb4e6f206c5a7b56c6ae05f
Raul2811 <raul.serrano01@up.ac.pa>
2024-08-15 12:58:49 -0500

    Se agregaron los modelos de las tablas: submission_Downloads,Context_Downloads,Month_Downloads

--- vae/database/models.py
+++ vae/database/models.py
@@ -18,4 +18,28 @@ class Statistics (Base):
     def __repr__(self):
         return f"<id=(id={self.id}, name='{self.name}', total='{self.total}', updated_at='{self.updated_at}'))>" # Representación de la tabla
 
+class Submission_Downloads(Base):
+    __tablename__ = 'submission_downloads' # Nombre de la tabla
+    submission_id = Column(BigInteger, ForeignKey('submissions.id'), nullable=False)
+    total_metric = Column(BigInteger, nullable=False)
 
+    def __repr__(self):
+        return f"<submission_id=(submission_id={self.submission_id}, total='{self.total_metric}'))>" # Representación de la tabla
+
+class Context_Downloads(Base):
+    __tablename__ = 'context_downloads' # Nombre de la tabla
+    context_id = Column(BigInteger, ForeignKey('contexts.id'), nullable=False)
+    total_metric = Column(BigInteger, nullable=False)
+
+    def __repr__(self):
+        return f"<context_id=(context_id={self.context_id}, total='{self.total_metric}'))>" # Representación de la tabla
+
+class Month_Downloads(Base):
+    __tablename__ = 'month_downloads' # Nombre de la tabla
+    month = Column(BigInteger, nullable=False)
+    total_metric = Column(BigInteger, nullable=False)
+
+    def __repr__(self):
+        return f"<month=(month={self.month}, total='{self.total_metric}'))>" # Representación de la tabla
+    
+    
