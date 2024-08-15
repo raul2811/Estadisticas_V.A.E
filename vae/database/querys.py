ef201eed3710e54822418a19e7b1e56940e599f0
Raul2811 <raul.serrano01@up.ac.pa>
2024-08-15 12:36:39 -0500

    se creo una funcion para mostrar el total de la estadisticas ingresando una variable name

--- vae/database/querys.py
+++ vae/database/querys.py
@@ -3,10 +3,11 @@ from typing import List
 
 
 class Querys_return: # Clase para retornar los querys osea los resultados de cada una de la consultasks
+    ''' #motivo de depuracion
     def querys():
-        Querys_return.mostrar_info()      
-
-    def mostrar_info ():
+        Querys_return.mostrar_colum_variables()
+    
+    def mostrar_colum_variables ():
         with Session() as session:
             try:
                 cont = 0
@@ -14,6 +15,15 @@ class Querys_return: # Clase para retornar los querys osea los resultados de cad
                 registro = session.query(Statistics).all()
                 for i in registro:
                     cont += 1
-                    print(f'{cont}. {i.name}')              
+                    registro= session.query(Statistics).filter(Statistics.name == i.name).first()
+                    print(f'{cont}. {i.name} . {registro.total}')
+            except Exception as e:
+                print(f"Ocurrió un error inesperado: {e}")
+    '''
+    def mostrar_colum_total (name):# Mostrar el total de la estadistica ingresando una variable name para retornar el valor de la columna total.
+        with Session() as session:
+            try:
+               registro= session.query(Statistics).filter(Statistics.name == name).first()
+               return registro.total
             except Exception as e:
                 print(f"Ocurrió un error inesperado: {e}")
