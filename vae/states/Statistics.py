b9789fa219ae649e3d2c3d4720f5b1510aeba0fd
Raul2811 <raul.serrano01@up.ac.pa>
2024-08-15 15:00:48 -0500

    Se modifico el retorno para que contenga los decimales

--- vae/states/Statistics.py
+++ vae/states/Statistics.py
@@ -4,29 +4,36 @@ from database.querys import Querys_return
 class Totals(rx.State):
     @rx.var
     def journals_total(self) -> str:
-        return Querys_return.mostrar_colum_total('journals')
+        total=Querys_return.mostrar_colum_total('journals')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de revistas.
     @rx.var
     def volumen_total(self) -> str:
-        return Querys_return.mostrar_colum_total('volumen')
+        total=Querys_return.mostrar_colum_total('volumen')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de volumenes de revistas en general.
     @rx.var
     def arti_ess_total(self) -> str:
-        return Querys_return.mostrar_colum_total('arti_ess')
+        total=Querys_return.mostrar_colum_total('arti_ess')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  articulos y ensayos.
     @rx.var
     def users_total(self) -> str:
-        return Querys_return.mostrar_colum_total('users')
+        total=Querys_return.mostrar_colum_total('users')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios general.
     @rx.var
     def users_pa_total(self) -> str:
-        return Querys_return.mostrar_colum_total('users_pa')
+        total=Querys_return.mostrar_colum_total('users_pa')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios panamenos.
     @rx.var
     def users_ext_total(self) -> str:
-        return Querys_return.mostrar_colum_total('users_ext')
+        total=Querys_return.mostrar_colum_total('users_ext')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios extranjeros.
     @rx.var
     def downloads_total(self) -> str:
-        return Querys_return.mostrar_colum_total('downloads')
+        total=Querys_return.mostrar_colum_total('downloads')
+        return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  descargas.
