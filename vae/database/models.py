0ca789fd5e0ac7d8f089ebabfcae2e2075ecc27c
angelcruzlasso <angelcruzlasso@gmail.com>
2024-08-15 18:22:28 -0500

    se agrge un nuevo modelo (tabla) enfocada en consultar los 4 mas descargado

--- vae/database/models.py
+++ vae/database/models.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from sqlalchemy import Column, String, Float, BigInteger, Integer, ForeignKey
 from sqlalchemy.orm import sessionmaker ,declarative_base,relationship
 from database.engine import engine
@@ -41,3 +42,16 @@ class Month_Downloads(Base):
 
     def __repr__(self):
         return f"<month=(month={self.month}, total='{self.total_metric}'))>" # Representación de la tabla
+
+class Submission_downloads_top (Base):
+    __tablename__ = 'submission_downloads_top' # Nombre de la tabla
+    submission_id = Column(BigInteger,primary_key=True, nullable=False)
+    context_id = Column(BigInteger, nullable=False)
+    path = Column(String(32), nullable=False)
+    publication_id = Column(BigInteger, nullable=False)
+    cleantitle = Column(String(32), nullable=False)
+    issueid = Column(Integer, nullable=False)
+    total_metric = Column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"<submission_id=(submission_id={self.submission_id}, context_id='{self.context_id}', path='{self.path}', publication_id='{self.publication_id}', cleantitle='{self.cleantitle}', issueid='{self.issueid}', total_metric='{self.total_metric}'))>" # Representación de la tabla
