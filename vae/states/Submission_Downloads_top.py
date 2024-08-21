264a2e4b9f87878fb70b1da2021383496e47c846
Raul2811 <raul.serrano01@up.ac.pa>
2024-08-21 12:18:00 -0500

    se itento realizar el retorno de los dato mas sin embargo hay un error a la hora de obtener el indice

---
+++ vae/states/Submission_Downloads_top.py
@@ -0,0 +1,176 @@
+import reflex as rx
+from database.querys import Querys_return
+
+class Submission_Downloads_top(rx.State):
+    @rx.var
+    def context_id_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'context_id')
+        return data
+        #retorna el context_id de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def submission_id_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'submission_id')
+        return data
+        #retorna el submission_id de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def path_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'path')
+        return data
+        #retorna el path de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def publication_id_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'publication_id')
+        return data
+        #retorna el publication_id de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def clean_title_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'clean_title')
+        return data
+        #retorna el clean_title de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def issue_id_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'issue_id')
+        return data
+        #retorna el issue_id de el articulo que mas se ha descargado en la posicion 0
+    @rx.var
+    def total_metric_0(self):
+        self=0
+        data=Querys_return.mostrar_submission_download_top(self,'total_metric')
+        return data
+        #retorna el total_metric de el articulo que mas se ha descargado en la posicion 0
+    #------------------------------------------
+    @rx.var
+    def context_id_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'context_id')
+        return data
+        #retorna el context_id de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def submission_id_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'submission_id')
+        return data
+        #retorna el submission_id de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def path_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'path')
+        return data
+        #retorna el path de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def publication_id_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'publication_id')
+        return data
+        #retorna el publication_id de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def clean_title_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'clean_title')
+        return data
+        #retorna el clean_title de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def issue_id_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'issue_id')
+        return data
+        #retorna el issue_id de el articulo que mas se ha descargado en la posicion 1
+    @rx.var
+    def total_metric_1(self):
+        self=1
+        data=Querys_return.mostrar_submission_download_top(self,'total_metric')
+        return data
+        #retorna el total_metric de el articulo que mas se ha descargado en la posicion 1
+    #------------------------------------------
+    @rx.var
+    def context_id_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'context_id')
+        return data
+        #retorna el context_id de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def submission_id_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'submission_id')
+        return data
+        #retorna el submission_id de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def path_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'path')
+        return data
+        #retorna el path de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def publication_id_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'publication_id')
+        return data
+        #retorna el publication_id de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def clean_title_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'clean_title')
+        return data
+        #retorna el clean_title de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def issue_id_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'issue_id')
+        return data
+        #retorna el issue_id de el articulo que mas se ha descargado en la posicion 2
+    @rx.var
+    def total_metric_2(self):
+        self=2
+        data=Querys_return.mostrar_submission_download_top(self,'total_metric')
+        return data
+        #retorna el total_metric de el articulo que mas se ha descargado en la posicion 2
+    #------------------------------------------
+    @rx.var
+    def context_id_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'context_id')
+        return data
+        #retorna el context_id de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def submission_id_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'submission_id')
+        return data
+        #retorna el submission_id de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def path_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'path')
+        return data
+        #retorna el path de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def publication_id_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'publication_id')
+        return data
+        #retorna el publication_id de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def clean_title_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'clean_title')
+        return data
+        #retorna el clean_title de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def issue_id_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'issue_id')
+        return data
+        #retorna el issue_id de el articulo que mas se ha descargado en la posicion 3
+    @rx.var
+    def total_metric_3(self):
+        self=3
+        data=Querys_return.mostrar_submission_download_top(self,'total_metric')
+        return data
+        #retorna el total_metric de el articulo que mas se ha descargado en la posicion 3
+    
