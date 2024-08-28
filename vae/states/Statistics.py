8b0db16597caf93dd76c64b7f48aaf2ad98eb519
Raul2811 <raul.serrano01@up.ac.pa>
2024-08-28 08:10:30 -0500

    se le agrego al decoradro rx.var el argumento cache=True , para probar se removio en vista que no actualizaba la variable en tiempo real

--- vae/states/Statistics.py
+++ vae/states/Statistics.py
@@ -2,37 +2,37 @@ import reflex as rx
 from database.querys import Querys_return
 
 class Totals(rx.State):
-    @rx.var(cache=True)
+    @rx.var()
     def journals_total(self) -> str:
         total=Querys_return.mostrar_statistics('journals')
         return total
         #retorna el total de revistas.
-    @rx.var(cache=True)
+    @rx.var()
     def volumen_total(self) -> str:
         total=Querys_return.mostrar_statistics('volumen')
         return f"{total:,.2f}".replace(",", ".")
         #retorna el total de volumenes de revistas en general.
-    @rx.var(cache=True)
+    @rx.var()
     def arti_ess_total(self) -> str:
         total=Querys_return.mostrar_statistics('arti_ess')
         return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  articulos y ensayos.
-    @rx.var(cache=True)
+    @rx.var()
     def users_total(self) -> str:
         total=Querys_return.mostrar_statistics('users')
         return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios general.
-    @rx.var(cache=True)
+    @rx.var()
     def users_pa_total(self) -> str:
         total=Querys_return.mostrar_statistics('users_pa')
         return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios panamenos.
-    @rx.var(cache=True)
+    @rx.var()
     def users_ext_total(self) -> str:
         total=Querys_return.mostrar_statistics('users_ext')
         return f"{total:,.2f}".replace(",", ".")
         #retorna el total de  usuarios extranjeros.
-    @rx.var(cache=True)
+    @rx.var()
     def downloads_total(self) -> str:
         total=Querys_return.mostrar_statistics('downloads')
         return f"{total:,.2f}".replace(",", ".")
